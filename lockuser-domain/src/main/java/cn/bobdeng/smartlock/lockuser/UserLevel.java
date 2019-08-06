effdc0c919e41b1e283bae77b8130a869f1666b4
DengZhiGuo <bobdeng@163.com>
2019-08-06 11:37:46 +0800

    add description

--- lockuser-domain/src/main/java/cn/bobdeng/smartlock/lockuser/UserLevel.java
+++ lockuser-domain/src/main/java/cn/bobdeng/smartlock/lockuser/UserLevel.java
@@ -4,11 +4,13 @@ package cn.bobdeng.smartlock.lockuser;
 import java.util.stream.Stream;
 
 public enum UserLevel {
-    OWNER(1000), ADMIN(100), ADVANCED(50), NORMAL(10);
+    OWNER(1000,"拥有者"), ADMIN(100,"管理员"), ADVANCED(50,"高级用户"), NORMAL(10,"普通用户");
     int value;
+    String description;
 
-    UserLevel(int value) {
+    UserLevel(int value, String description) {
         this.value = value;
+        this.description = description;
     }
 
     public static UserLevel of(int level) {
