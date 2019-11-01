4af615e1c80b0452c771a164ee955499ca1fdade
DengZhiGuo <bobdeng@163.com>
2019-11-01 19:42:29 +0800

    add max fingerpass

--- lockuser-domain/src/main/java/cn/bobdeng/smartlock/lockuser/UserLevel.java
+++ lockuser-domain/src/main/java/cn/bobdeng/smartlock/lockuser/UserLevel.java
@@ -1,21 +1,22 @@
 package cn.bobdeng.smartlock.lockuser;
 
 
-
 import java.util.stream.Stream;
 
 public enum UserLevel {
-    OWNER(1000,"拥有者"), ADMIN(100,"管理员"), ADVANCED(50,"高级用户"), NORMAL(10,"普通用户");
+    OWNER(1000, "拥有者", 10), ADMIN(100, "管理员", 5), ADVANCED(50, "高级用户",2), NORMAL(10, "普通用户",0);
     int value;
     String description;
+    int maxFingerPass;
 
     public String getDescription() {
         return description;
     }
 
-    UserLevel(int value, String description) {
+    UserLevel(int value, String description, int maxFingerPass) {
         this.value = value;
         this.description = description;
+        this.maxFingerPass = maxFingerPass;
     }
 
 
